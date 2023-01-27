7fb5ea0b188614fa89d78364d914940e2dbf2a00
AbdulHadi <abdulhadi91478@gmail.com>
2023-01-28 00:48:43 +0500

    do it

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+// function generate(){
+//     let x = document.getElementById("quotes").innerHTML =" UNDERSTAND THAT CONSISTENCY IS ESSENTIAL ";
+//     document.getElementById("bld").innerHTML = x;
+// }
+
 function generate(){
-    let x = document.getElementById("quotes").innerHTML =
-    {+" Understand that Consistency is essential ";
-    document.getElementById("bld").innerHTML = x;
+    let x = [
+        {
+            author: "mustafa",
+            quote: "good boy"
+        },
+        {
+            author: "hadi",
+            quote: "good boy at all"
+        },
+        {
+            author: "ali",
+            quote: "good boy at all time"
+        }
+
+    ];
+
+const lenghtOfQuote = x
+let y = (Math.floor(Math.random()));   
+document.getElementById("quote").innerHTML = 
+
 }
